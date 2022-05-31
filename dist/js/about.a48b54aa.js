1e481e403678a0736c9b2a1df394da5747ad0445
matrice-one <neigeline.rivollat@gmail.com>
2022-05-31 14:31:02 +0200

    okcchange dist

---
+++ dist/js/about.a48b54aa.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,a){"use strict";a.r(t);var e=a("7a23"),u={class:"about"},c=Object(e["h"])("h1",null,"This is an about page",-1),o=[c];function s(n,t){return Object(e["u"])(),Object(e["g"])("div",u,o)}var b=a("6b0d"),i=a.n(b);const r={},d=i()(r,[["render",s]]);t["default"]=d}}]);
+//# sourceMappingURL=about.a48b54aa.js.map
