7c12af442d3eca3feedf5c34ef7ccc31f7310957
alqua <bilatli@hotmail.com>
2022-06-08 16:42:14 +0200

    change server origin for local prod

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'https://api.elona-construction.ch'  
-// axios.defaults.baseURL = 'http://127.0.0.1:8000'  
+// axios.defaults.baseURL = 'https://api.elona-construction.ch'  
+ axios.defaults.baseURL = 'http://127.0.0.1:8000'  
 
 createApp(App).use(store).use(router,axios).mount('#app')
